5fc797cf68f2461ee104a8d1936ce9938ab5c879
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-08-18 21:00:04 +0300

    Add initial version of files

---
+++ server.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'sinatra'
+require 'json'
+
+get '/api/site/:site' do
+  visit(params['site']).to_json
+end
+
+def visit(site)
+  file = File.read('./data.json')
+  data = JSON.parse(file)
+  amount = if data.has_key? site
+             data[site]['amount'] + 1
+           else
+             data[site] = Hash.new
+             data[site]['since'] = Time.now.to_i
+             1
+           end
+  data[site]['amount'] = amount
+  File.write('./data.json', JSON.dump(data))
+  data[site]
+end
